1abdc62a2da90c1729764a37f34f594a427ff150
propertybridgesupport-dot <propertybridgesupport@gmail.com>
2026-09-17 13:00:26 -0500

    Route OAuth requests to consent screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import OAuthConsent from './OAuthConsent.jsx'
 import './styles.css'
+
+const isOAuthConsent = window.location.pathname === '/oauth/consent'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isOAuthConsent ? <OAuthConsent /> : <App />}
   </React.StrictMode>
 )
-  if ('serviceWorker' in navigator) {
+
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
       .then(() => console.log('Service Worker registered'))
